77b757c2689374c15beffd049402adcac82cfba2
Ana Lydia Moreira <analydiamoreira965@gmail.com>
2023-01-31 22:06:36 -0300

    Revisao MVC

---
+++ src/RevisaoProjetoNoticias.Web/Controllers/CategoryController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RevisaoProjetoNoticias.Domain.IServices;
+using RevisaoProjetoNoticias.Web.Models;
+using System.Diagnostics;
+
+namespace RevisaoProjetoNoticias.Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ILogger<CategoryController> _logger;
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ILogger<CategoryController> logger,
+            ICategoryService categoryService)
+        {
+            _logger = logger;
+            _categoryService = categoryService;
+        }
+
+        public IActionResult Index()
+        {
+
+            //To list all categories
+            // Get of CategoryService through DI (ICategoryService)
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+    }
+}
